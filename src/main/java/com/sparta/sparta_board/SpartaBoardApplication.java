95274e51a9f5193476ca726995ee61ce9604760d
gk <iba3374@naver.com>
2023-11-03 16:57:06 +0900

    게시글 등록 구현

--- src/main/java/com/sparta/sparta_board/SpartaBoardApplication.java
+++ src/main/java/com/sparta/sparta_board/SpartaBoardApplication.java
@@ -2,8 +2,10 @@ package com.sparta.sparta_board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpartaBoardApplication {
 
 	public static void main(String[] args) {
---
+++ src/test/java/com/sparta/sparta_board/service/BoardServiceTest.java
@@ -0,0 +1,38 @@
+package com.sparta.sparta_board.service;
+
+import com.sparta.sparta_board.entity.Board;
+import com.sparta.sparta_board.entity.BoardRequestDto;
+import com.sparta.sparta_board.entity.BoardResponseDto;
+import com.sparta.sparta_board.repository.BoardRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class BoardServiceTest {
+
+    @Autowired
+    private BoardService boardService;
+
+    @Autowired
+    private BoardRepository boardRepository;
+    
+    @Test
+    @DisplayName("게시글 생성 테스트")
+    void test1() {
+        BoardRequestDto request = new BoardRequestDto("제목", "1234", "userA", "내용");
+        BoardResponseDto response = boardService.createBoard(request);
+
+        Board findBoard = boardRepository.findById(response.getId()).get();
+        Assertions.assertThat(findBoard.getId()).isEqualTo(response.getId());
+
+        Assertions.assertThat(findBoard.getUsername()).isEqualTo(request.getUsername());
+        Assertions.assertThat(findBoard.getTitle()).isEqualTo(request.getTitle());
+        Assertions.assertThat(findBoard.getContent()).isEqualTo(request.getContent());
+        Assertions.assertThat(findBoard.getPassword()).isEqualTo(request.getPassword());
+    }
+}
