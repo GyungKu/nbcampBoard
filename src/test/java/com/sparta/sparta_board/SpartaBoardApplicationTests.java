476f6e4801073f4bf19f5f8be829c90f64e4aa85
gk <iba3374@naver.com>
2023-11-03 18:46:53 +0900

    Apply .gitignore

---
+++ src/test/java/com/sparta/sparta_board/SpartaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.sparta_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
