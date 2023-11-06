4911bb1ef38404816a148285a0d7445932dc9c03
gk <iba3374@naver.com>
2023-11-06 15:32:24 +0900

    반환을 ResponseEntity로 변경

--- src/main/java/com/sparta/sparta_board/controller/BoardController.java
+++ src/main/java/com/sparta/sparta_board/controller/BoardController.java
@@ -7,6 +7,7 @@ import com.sparta.sparta_board.exception.PasswordException;
 import com.sparta.sparta_board.service.BoardService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.ResponseEntity.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,54 +27,54 @@ public class BoardController {
     private final BoardService boardService;
 
     @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseStatus(NOT_FOUND)
-    public GlobalResponseDto illegalArgsHandler(IllegalArgumentException e) {
-        return new GlobalResponseDto("not found", e.getMessage());
+    public ResponseEntity<GlobalResponseDto> illegalArgsHandler(IllegalArgumentException e) {
+        return status(NOT_FOUND).body(new GlobalResponseDto("not found", e.getMessage()));
     }
 
     @ExceptionHandler(PasswordException.class)
-    @ResponseStatus(UNAUTHORIZED)
-    public GlobalResponseDto passwordHandler(PasswordException e) {
-        return new GlobalResponseDto("invalid password", e.getMessage());
+    public ResponseEntity<GlobalResponseDto> passwordHandler(PasswordException e) {
+        return status(UNAUTHORIZED).body(new GlobalResponseDto("invalid password", e.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(BAD_REQUEST)
-    public GlobalResponseDto validationExceptionHandler(MethodArgumentNotValidException e) {
+    public ResponseEntity<GlobalResponseDto> validationExceptionHandler(MethodArgumentNotValidException e) {
         HashMap<String, String> errors = new HashMap<>();
         e.getAllErrors().forEach(error -> errors.put(((FieldError) error).getField(), error.getDefaultMessage()));
-        return new GlobalResponseDto("validation error", errors);
+
+        return badRequest().body(new GlobalResponseDto("validation error", errors));
     }
 
+
+
     @PostMapping("/board")
-    public GlobalResponseDto createBoard(@RequestBody @Valid BoardRequestDto boardRequestDto) {
+    public ResponseEntity<GlobalResponseDto> createBoard(@RequestBody @Valid BoardRequestDto boardRequestDto) {
         BoardResponseDto board = boardService.createBoard(boardRequestDto);
-        return new GlobalResponseDto("create success", board);
+        return status(CREATED).body(new GlobalResponseDto("create success", board));
     }
 
     @GetMapping("/board/{id}")
-    public GlobalResponseDto getBoard(@PathVariable Long id) {
+    public ResponseEntity<GlobalResponseDto> getBoard(@PathVariable Long id) {
         BoardResponseDto board = boardService.getBoard(id);
-        return new GlobalResponseDto("board", board);
+        return ok(new GlobalResponseDto("board", board));
     }
 
     @GetMapping("/boards")
-    public GlobalResponseDto getBoards() {
+    public ResponseEntity<GlobalResponseDto> getBoards() {
         List<BoardResponseDto> boards = boardService.getBoards();
-        return new GlobalResponseDto("board list", boards);
+        return ok(new GlobalResponseDto("board list", boards));
     }
 
     @PutMapping("/board/{id}")
-    public GlobalResponseDto updateBoard(@PathVariable Long id, @RequestBody @Valid BoardRequestDto boardRequestDto) {
+    public ResponseEntity<GlobalResponseDto> updateBoard(@PathVariable Long id, @RequestBody @Valid BoardRequestDto boardRequestDto) {
         BoardResponseDto board = boardService.updateBoard(id, boardRequestDto);
-        return new GlobalResponseDto("update success", board);
+        return ok(new GlobalResponseDto("update success", board));
     }
 
     @DeleteMapping("/board/{id}")
-    public GlobalResponseDto deleteBoard(@PathVariable Long id, @RequestBody Map<String, String> passwordMap) {
+    public ResponseEntity<GlobalResponseDto> deleteBoard(@PathVariable Long id, @RequestBody Map<String, String> passwordMap) {
         String password = passwordMap.get("password");
         boardService.deleteBoard(id, password);
-        return new GlobalResponseDto("delete success", id);
+        return ok(new GlobalResponseDto("delete success", id));
     }
 
 }
